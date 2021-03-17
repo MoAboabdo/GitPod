53743ac94a1ef998f46a339714ccb3128c1b6c2f
MoAboabdo <m.aboabdo@yahoo.com>
2021-03-17 15:54:54 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+
+
+const app = express();
+
+
+app.use(express.json());
+
+
+
+
+const port = process.env.port || 3000;
+
+app.listen(port,()=>{
+    console.log('server running.....')
+});
